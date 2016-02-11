3e18ab97dcbd95e48bce7f065b5638c6cca39334
Kale Franz <kfranz@continuum.io>
2016-02-10 23:22:47 -0600

    full monty

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -3,6 +3,8 @@ conda info
 # PKG_VERSION=0.0.0
 # CPU_COUNT=8
 
+TOKEN=ae-3f82bb6b-d33f-43d1-b8f1-a14e2ae1c1ed
+
 pkg_names="$(conda list --canonical --no-pip)"
 
 conda_pkgs_dir="$(conda info 2> /dev/null | grep "package cache" | cut -d: -f2 | tr -d '[[:space:]]')"
@@ -11,13 +13,4 @@ echo $conda_pkgs_dir
 
 pushd $conda_pkgs_dir
 
-conda list --canonical --no-pip --name _build | xargs -I{} -n1 ls -al {}.tar.bz2
-
-conda list --canonical --no-pip --name _build | xargs -t -I{} -n1 anaconda --token ae-3f82bb6b-d33f-43d1-b8f1-a14e2ae1c1ed upload --user aetrial --label $PKG_VERSION {}.tar.bz2
-
-# xargs -t -I{} -n1 -P$CPU_COUNT anaconda upload --user aetrial --label $PKG_VERSION {}.tar.bz2
-
-# anaconda upload --register
-
-
-
+conda list --canonical --no-pip --name _build | xargs -t -I{} -n1 -P$CPU_COUNT anaconda --token $TOKEN upload --user aetrial --label $PKG_VERSION {}.tar.bz2
