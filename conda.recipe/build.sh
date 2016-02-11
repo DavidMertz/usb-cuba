81db62445c972221e76d79e44c8f6da2e0a977d2
Kale Franz <kfranz@continuum.io>
2016-02-10 21:40:27 -0600

    add binstar.yml

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -7,9 +7,12 @@ pkg_names="$(conda list --canonical --no-pip)"
 
 conda_pkgs_dir="$(conda info 2> /dev/null | grep "package cache" | cut -d: -f2 | tr -d '[[:space:]]')"
 
+echo $conda_pkgs_dir
+
 pushd $conda_pkgs_dir
 
 conda list --canonical --no-pip --name _build | xargs -I{} -n1 ls -al {}.tar.bz2
 
 # xargs -t -I{} -n1 -P$CPU_COUNT anaconda upload --user aetrial --label $PKG_VERSION {}.tar.bz2
 
+# anaconda upload --register
