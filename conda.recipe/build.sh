0e33feb693a840bd70d73a7028fb436f9c85c7bd
Kale Franz <kfranz@continuum.io>
2016-02-10 21:29:13 -0600

    first commit

---
+++ conda.recipe/build.sh
@@ -0,0 +1,15 @@
+printenv
+conda info
+# PKG_VERSION=0.0.0
+# CPU_COUNT=8
+
+pkg_names="$(conda list --canonical --no-pip)"
+
+conda_pkgs_dir="$(conda info 2> /dev/null | grep "package cache" | cut -d: -f2 | tr -d '[[:space:]]')"
+
+pushd $conda_pkgs_dir
+
+conda list --canonical --no-pip --name _build | xargs -I{} -n1 ls -al {}.tar.bz2
+
+# xargs -t -I{} -n1 -P$CPU_COUNT anaconda upload --user aetrial --label $PKG_VERSION {}.tar.bz2
+
