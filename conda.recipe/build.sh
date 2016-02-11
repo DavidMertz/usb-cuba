f0191981e8821c387056bc1b263266cb007dcfa6
Kale Franz <kfranz@continuum.io>
2016-02-10 23:05:17 -0600

    token in wrong place?

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -13,8 +13,12 @@ pushd $conda_pkgs_dir
 
 conda list --canonical --no-pip --name _build | xargs -I{} -n1 ls -al {}.tar.bz2
 
-conda list --canonical --no-pip --name _build | xargs -t -I{} -n1 anaconda upload -t ae-2e752bfc-adc5-4f7c-ba72-05902bc95bf9 --user aetrial --label $PKG_VERSION {}.tar.bz2
+conda list --canonical --no-pip --name _build | xargs -t -I{} -n1 anaconda --token ae-2e752bfc-adc5-4f7c-ba72-05902bc95bf9 upload --user aetrial --label $PKG_VERSION {}.tar.bz2
 
 # xargs -t -I{} -n1 -P$CPU_COUNT anaconda upload --user aetrial --label $PKG_VERSION {}.tar.bz2
 
 # anaconda upload --register
+
+
+
+anaconda auth --create --name anacondacloud -o aetrial -s 'api:build' --url anaconda.org
